cceacfb50b441a819e3b53fae44612f4fdd881cb
Muhammad Waleed Tahir <73441359+tahirwaleed399@users.noreply.github.com>
2023-04-09 16:50:26 +0500

    Initial Commit

---
+++ 1st class.js
@@ -0,0 +1,22 @@
+console.log('Hello World');
+
+
+// old usage with var
+// var name = "Waleeed";
+// var age =  17;
+
+// ctrl + / to comment  
+// Modern usage with let and const
+
+// const name = "Waleed";
+// const name = 'waleed';
+// let age = 17 ;
+
+
+// error 
+name = "Haris";
+age=18;
+
+console.log(name);
+console.log(age);
+
---
+++ script.js
@@ -0,0 +1,14 @@
+let a = 32;
+
+// 1 : number ;
+// 2 : string ;
+// 3 : boolean ;
+// 4 : undefined;
+// 5 : null ;
+// 6 : object ;
+// 7 : Symbol ;
+
+
+
+console.log(a)
+console.log(typeof a)
